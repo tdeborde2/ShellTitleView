4f240e6829c8ce17b41b9a045691b1aebd7c2adf
Deborde, Timothy (T.) <tdebord1@ford.com>
2022-08-08 12:57:34 -0400

    Initial commit

---
+++ ShellTitleView/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShellTitleView;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ ShellTitleView/TitleView.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ShellTitleView;
+
+public partial class TitleView : ContentView
+{
+	public TitleView()
+	{
+		InitializeComponent();
+	}
+}
